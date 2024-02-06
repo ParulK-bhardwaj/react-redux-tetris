3476d67e10485e8722d8abcfadf1f65a61fcca9c
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2024-02-05 23:31:17 -0500

    Added grid square

---
+++ src/components/GridSquare.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Represents a grid square with a color
+
+export default function GridSquare({ color }) {
+  const classes = `grid-square color-${color}`
+  return (
+  <div className={ classes }>
+    
+  </div>
+  );
+}
